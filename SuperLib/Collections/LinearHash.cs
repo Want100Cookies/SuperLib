693a6d0295524825c1e69f6d41185de6034e9746
Want100Cookies <pascal@drewes-webdesign.nl>
2016-03-08 16:30:51 +0100

    Added LinearHash

---
+++ SuperLib/Collections/LinearHash.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLib.Collections
+{
+    public class LinearHash<T> : IEnumerable<T>
+    {
+        private const int Size = 101;
+
+        private T[] Data;
+
+        public LinearHash()
+        {
+            Data = new T[Size];
+        }
+
+        public T this[int index] => Data[index];
+
+        public void Insert(T item)
+        {
+            int hash = item.GetHashCode();
+
+            if (Data[hash].Equals(default(T)))
+            {
+                Data[hash] = item;
+            }
+            else
+            {
+                int tries = 1;
+                bool saved = false;
+
+                while (!saved)
+                {
+                    if (!Data[hash + tries].Equals(default(T)))
+                    {
+                        tries++;
+                    }
+                    else
+                    {
+                        Data[hash + tries] = item;
+                        saved = true;
+                    }
+                }
+            }
+        }
+
+        public void Remove(T item)
+        {
+            int hash = item.GetHashCode();
+
+            if (Data[hash].Equals(item))
+            {
+                Data[hash] = default(T);
+            }
+            else
+            {
+                int tries = 1;
+                bool saved = false;
+
+                while (!saved)
+                {
+                    if (!Data[hash + tries].Equals(item))
+                    {
+                        tries++;
+                    }
+                    else
+                    {
+                        Data[hash + tries] = default(T);
+                        saved = true;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in Data)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
