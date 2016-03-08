e4f14210d7168e7e10d6a5a4cbe78141d061b973
Want100Cookies <pascal@drewes-webdesign.nl>
2016-03-08 10:09:22 +0100

    Improved smart bubble sort

--- SuperLib/Sort/SmartBubbleSort.cs
+++ SuperLib/Sort/SmartBubbleSort.cs
@@ -3,30 +3,27 @@ using SuperLib.Collections;
 
 namespace SuperLib.Sort
 {
-    // I suppose this is the smartbubble that BlackBoard says to be in the DLL
     public static class SmartBubbleSort
     {
         public static void Sort<T>(ref T[] arr) where T : IComparable<T>
         {
             T temp;
-            T[] oldArr;
+            bool hasChanged;
 
             for (int outer = arr.Length - 1; outer >= 1; outer--)
             {
-                oldArr = (T[])arr.Clone();
+                hasChanged = false;
                 for (int inner = 0; inner <= outer - 1; inner++)
                 {
-                    if (arr[inner].CompareTo(arr[inner + 1]) > 0) // CompareTo less than zero: preceeds, zero: same spot, more than zero: follows this object
-                    {
-                        temp = arr[inner];
-                        arr[inner] = arr[inner + 1];
-                        arr[inner + 1] = temp;
-                    }
+                    if (arr[inner].CompareTo(arr[inner + 1]) <= 0) continue;
+
+                    temp = arr[inner];
+                    arr[inner] = arr[inner + 1];
+                    arr[inner + 1] = temp;
+
+                    hasChanged = true;
                 }
-                if (oldArr.Equals(arr))
-                {
-                    return;
-                }
+                if (!hasChanged) return;
             }
         }
     }
