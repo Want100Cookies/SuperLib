7e8ae5ee416bc88ba8a4368b33fb2e1f4e4d3d22
Want100Cookies <pascal@drewes-webdesign.nl>
2016-03-15 09:59:45 +0100

    Added LinkedList

---
+++ SuperLib/Collections/LinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLib.Collections
+{
+    class LinkedList<T>
+    {
+        private readonly Node<T> _header;
+
+        public LinkedList()
+        {
+            _header = new Node<T>(default(T));
+        }
+
+        public void Insert(T newItem, T after)
+        {
+            Node<T> current = Find(after);
+            Node<T> newNode = new Node<T>(newItem);
+
+            newNode.Link = current.Link;
+            current.Link = newNode;
+        }
+
+        public void InsertFirst(T newItem)
+        {
+            Node<T> newNode = new Node<T>(newItem);
+
+            _header.Link = newNode;
+            newNode.Link = _header;
+        }
+
+        public void Remove(T item)
+        {
+            Node<T> toRemove = FindPrevious(item);
+
+            if (toRemove.Link != null)
+            {
+                toRemove.Link = toRemove.Link.Link;
+            }
+        }
+
+        private Node<T> Find(T item)
+        {
+            Node<T> current = _header;
+
+            while (!current.Element.Equals(item))
+            {
+                current = current.Link;
+            }
+
+            return current;
+        }
+
+        private Node<T> FindPrevious(T item)
+        {
+            Node<T> current = _header;
+
+            while (current.Link != null && !current.Link.Element.Equals(item))
+            {
+                current = current.Link;
+            }
+
+            return current;
+        }
+
+        public IEnumerable<T> GetEnumerable()
+        {
+            Node<T> current = _header;
+
+            while (!Equals(current.Link, default(T)))
+            {
+                current = current.Link;
+                if (!Equals(current.Element, default(T))) yield return current.Element;
+            }
+        }
+    }
+}
